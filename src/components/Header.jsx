82e6780f6f4b4c807b7d6654769cacccbb5cdaf7
ashleytsmith <ashley.thom.smith@gmail.com>
2023-01-12 15:30:30 +0100

    Animate the title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,26 @@
-import '../styles/Header.css';
+import '../styles/Header.scss';
+
+import { useEffect } from 'react';
+import { gsap } from "gsap";
 
 function Header() {
+
+  useEffect(function() {
+ 
+    gsap.to(
+      
+    ".title", 
+    { backgroundPosition: "-960px 0", duration: 30, repeat: -1 }
+    );
+   
+  }, []);
+
 
   return (
 
 
-    <div className="container-fluid p-5 my-5 text-center header">
-      <h1>Ashley</h1>
-      <h1>Developer and Scientist</h1>
+    <div className="container-fluid p-5 my-5 text-center">
+      <h1 className = "title"> Ashley Developer and Scientist</h1>
     </div>
 
 
